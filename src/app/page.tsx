aa3fe0b2f8c773b3a4286ebbc70aded7541d1a9f
vinokour <vinokour@umich.edu>
2023-06-05 13:16:46 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image'
-import Navbar from './components/navbar'
-import Picture from './components/picture'
-import Aboutbar from './components/aboutbar'
+import Image from "next/image";
+import Navbar from "./components/navbar";
+import Picture from "./components/picture";
+import Aboutbar from "./components/aboutbar";
 export default function Home() {
   return (
     <div className=" bg-[#ffff] w-full h-[100vh]">
@@ -9,16 +9,13 @@ export default function Home() {
       <div className="flex flex-col w-full h-[50vh] bg-white-400">
         <div className="flex flex-row justify-center w-full py-10 mr-20 mt-10 text-black ">
           <div className="flex flex-col w-[50%] px-10 h-[50%]">
-          <Picture></Picture>   
+            <Picture></Picture>
           </div>
-
         </div>
-        <div className='flex flex-row bg-blue-400 w-full h-full py-15 mt-20'>
-            <Aboutbar></Aboutbar>
-
-          </div>
-    </div>
-    
+        <div className="flex flex-row bg-blue-400 w-full h-full py-15 mt-20">
+          <Aboutbar></Aboutbar>
+        </div>
       </div>
-  )
+    </div>
+  );
 }
