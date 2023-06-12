4141549b914e22c136a15823d38b062c192bb90c
vinokour <vinokour@umich.edu>
2023-06-12 12:54:20 -0400

    fixed logos

--- src/app/components/experience.tsx
+++ src/app/components/experience.tsx
@@ -1,10 +1,12 @@
 export default function Experience() {
   return (
-    <div className="flex flex-col gap-y-3 font-mono text-4xl text-blue-400 justify-between  text-center items-left bg-slate-800">
+    <div className="flex flex-col gap-y-10 h-full font-mono text-6xl text-blue-400 justify-between  text-center items-left bg-slate-800">
       Professional Experience
-      <p className="flex flex-row justify-center items-center gap-x-3">
-        <img className="object-contain h-10" src="soul4.png"></img>
-        <img src="UofM.png" className="object-contain h-10" />
+      <p className="flex flex-row justify-center items-center gap-x-10 gap-20">
+        <img className="object-contain px-10 h-20" src="soul4.png"></img>
+        <img src="Honeywell.png" className=" px-10 object-contain h-20" />
+        <img src="mlb.png" className=" px-10 object-contain h-20" />
+
       </p>
     </div>
   );
