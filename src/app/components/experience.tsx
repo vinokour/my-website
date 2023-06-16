d675d113b27611688d7e0414503a150673c4c7d0
IsaacJuracich <93289395+IsaacJuracich@users.noreply.github.com>
2023-06-15 19:11:59 -0500

    s

--- src/app/components/experience.tsx
+++ src/app/components/experience.tsx
@@ -12,62 +12,62 @@ export default function Experience() {
   };
 
   return (
-    <div className="flex flex-col gap-5 h-full lg:mt-20 py-6 font-mono text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-blue-400 mt-10 bg-slate-800 px-4 md:px-0">
-      <h1 className="text-center text-xl md:ml-10 md:text-3xl lg:text-5xl xl:text-7xl">
+    <div className="flex flex-col lg:items-center gap-5 h-full lg:mt-20 py-6 font-mono text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-blue-400 mt-10 bg-slate-800 px-4 md:px-0">
+      <div className="text-center md:ml-10 text-xl md:text-3xl lg:text-7xl">
         Professional Experience
-      </h1>
-      <div className="flex lg:flex-row flex-col flex-wrap gap-5 justify-center items-center lg:mt-6">
-        <div
-          className="flex flex-col items-center card rounded-3xl cursor-pointer bg-white p-4 md:p-3 lg:w-auto w-full"
-          onClick={() => handleClick("soul")}
-        >
-          <img
-            className="object-contain h-12"
-            src="soul4.png"
-            alt="Soul Stamps"
-          />
-          <p className="font-mono text-center text-sm text-black mt-4">
-            Software Engineer
-          </p>
-          <p className="font-mono text-center text-sm text-black">
-            October 2022 - Present
-          </p>
-        </div>
+        <div className="flex lg:flex-row flex-col flex-wrap gap-5 justify-center items-center lg:mt-6 w-full">
+          <div
+            className="flex flex-col items-center card rounded-3xl cursor-pointer bg-white p-4 md:p-3 lg:w-[29.5%] w-full h-[8rem] max-h-[8rem]"
+            onClick={() => handleClick("soul")}
+          >
+            <img
+              className="object-contain h-8"
+              src="soul4.png"
+              alt="Soul Stamps"
+            />
+            <p className="font-mono text-center text-sm text-black mt-4">
+              Software Engineer
+            </p>
+            <p className="font-mono text-center text-sm text-black">
+              October 2022 - Present
+            </p>
+          </div>
 
-        <div
-          className="flex flex-col items-center card rounded-3xl cursor-pointer bg-white p-4 lg:w-auto w-full"
-          onClick={() => handleClick("honeywell")}
-        >
-          <img
-            className="object-contain h-12"
-            src="Honeywell.png"
-            alt="Honeywell"
-          />
-          <p className="font-mono text-center text-sm text-black">
-            Software Engineer Intern
-          </p>
-          <p className="font-mono text-center text-sm text-black">
-            May 2023 - Present
-          </p>
-        </div>
+          <div
+            className="flex flex-col items-center card rounded-3xl cursor-pointer bg-white p-4 lg:w-[29.5%] w-full h-[8rem] max-h-[8rem]"
+            onClick={() => handleClick("honeywell")}
+          >
+            <img
+              className="object-contain h-10"
+              src="Honeywell.png"
+              alt="Honeywell"
+            />
+            <p className="font-mono text-center text-sm text-black">
+              Software Engineer Intern
+            </p>
+            <p className="font-mono text-center text-sm text-black">
+              May 2023 - Present
+            </p>
+          </div>
 
-        <div
-          className="flex flex-col items-center card rounded-3xl cursor-pointer bg-white p-4 lg:w-auto w-full"
-          onClick={() => handleClick("mlb")}
-        >
-          <img className="object-contain h-12" src="mlb.png" alt="MLB" />
-          <p className="font-mono text-center text-sm text-black mt-4">
-            Creative Studio Intern
-          </p>
-          <p className="font-mono text-center text-sm text-black">
-            June 2022 - August 2022
-          </p>
+          <div
+            className="flex flex-col items-center card rounded-3xl cursor-pointer bg-white p-4 lg:w-[29.5%] w-full h-[8rem] max-h-[8rem]"
+            onClick={() => handleClick("mlb")}
+          >
+            <img className="object-contain h-12" src="mlb.png" alt="MLB" />
+            <p className="font-mono text-center text-sm text-black mt-4">
+              Creative Studio Intern
+            </p>
+            <p className="font-mono text-center text-sm text-black">
+              June 2022 - August 2022
+            </p>
+          </div>
         </div>
       </div>
 
       {selected === "soul" && (
         <div className="flex flex-col justify-center items-center">
-          <p className="font-mono w-full lg:text-lg md:text-base md:ml-10 lg:w-[70%] text-gray-400 text-xs mt-6">
+          <p className="font-mono w-full lg:text-lg md:text-base md:ml-10 lg:w-2/3 text-gray-400 text-xs mt-6">
             Since October of my Sophomore year, I have been working on Soul.
             Soul is a digital identity authenticator built on Solana, and we are
             a small startup based in Los Angeles. At Soul, I have been deeply
@@ -83,7 +83,7 @@ export default function Experience() {
 
       {selected === "honeywell" && (
         <div className="flex flex-col justify-center items-center">
-          <p className="font-mono w-full lg:text-lg md:text-base md:ml-10 lg:w-[70%] text-gray-400 text-xs mt-6">
+          <p className="font-mono w-full lg:text-lg md:text-base md:ml-10 lg:w-2/3 text-gray-400 text-xs mt-6">
             Starting this May, I have been a Software Engineer intern at
             Honeywell in their Atlanta offices. I am working under Honeywell
             Connected Enterprises (HCE) with Logistics and Warehouse team,
@@ -95,7 +95,7 @@ export default function Experience() {
 
       {selected === "mlb" && (
         <div className="flex flex-col justify-center items-center">
-          <p className="font-mono w-full lg:text-lg md:text-base md:ml-10 lg:w-[70%] text-gray-400 text-xs mt-6">
+          <p className="font-mono w-full lg:text-lg md:text-base md:ml-10 lg:w-2/3 text-gray-400 text-xs mt-6">
             Last Summer I was a Creative Studio intern at Major League Baseball.
             During my time at MLB, I put my coding skills to use by creating a
             web application that made the process of reviewing live game tweets
--- src/app/components/picture.tsx
+++ src/app/components/picture.tsx
@@ -1,7 +1,7 @@
 export default function Picture() {
   return (
-    <div className="flex lg:items-center flex-col px-4 lg:mr-10 gap-5">
-      <div className="flex flex-col lg:ml-20">
+    <div className="flex lg:items-center flex-col px-4 gap-5">
+      <div className="flex flex-col">
         <p className="flex text-lg font-mono text-blue-400 md:text-xl lg:text-2xl">
           Hi, my name is{" "}
         </p>
@@ -12,32 +12,34 @@ export default function Picture() {
           and I build things for fun.
         </p>
       </div>
-      <p className="flex lg:ml-20 py-5 lg:w-3/4 lg:mt-10 w-full font-mono text-sm md:text-base lg:px-5 lg:text-lg items-center text-gray-400 mb-5 leading-normal md:leading-loose">
-        I am currently pursuing a junior at the University of Michigan, majoring
-        in Computer Science with a minor in Business. New technologies have
-        always intrigued me, not just for their innovative aspects but more
-        importantly, for their potential to reshape our world. I keep a keen eye
-        on emerging trends and advancements, and I am always eager to learn more
-        about the latest developments in the tech industry. Being a sports
-        enthusiast, I am particularly interested in sports analytics. The way
-        data-driven insights are changing the game fascinates me, and I am
-        always up for discussing the latest trends and innovations in this
-        field. And yes, nothing beats the thrill of watching Ohio State lose to
-        Michigan in any aspect of life. As far as the entertainment industry is
-        concerned, I am intrigued by the transformative potential of technology.
-        From revolutionizing content creation and distribution to reshaping
-        audience engagement, I believe there are still many untapped
-        opportunities to be explored. Currently, I am actively seeking SWE/AI
-        Summer internships for 2024, eager to apply my knowledge and skills in a
-        real-world setting. Outside of academics and career pursuits, I am
-        dedicated to developing products for Soul, a digital identity
-        verification platform. Working on this project has allowed me to apply
-        my technical skills in a practical setting, enhancing my understanding
-        of blockchain technology and its applications in the realm of identity
-        verification. I welcome any opportunity to connect and discuss my
-        interests or potential collaborations. I look forward to hearing from
-        you!
-      </p>
+      <div className="flex flex-col items-center w-full">
+        <p className="flex py-5 lg:w-2/3 w-full font-mono text-sm md:text-base lg:text-lg items-center text-gray-400 mb-5 leading-normal md:leading-loose">
+          I am currently pursuing a junior at the University of Michigan,
+          majoring in Computer Science with a minor in Business. New
+          technologies have always intrigued me, not just for their innovative
+          aspects but more importantly, for their potential to reshape our
+          world. I keep a keen eye on emerging trends and advancements, and I am
+          always eager to learn more about the latest developments in the tech
+          industry. Being a sports enthusiast, I am particularly interested in
+          sports analytics. The way data-driven insights are changing the game
+          fascinates me, and I am always up for discussing the latest trends and
+          innovations in this field. And yes, nothing beats the thrill of
+          watching Ohio State lose to Michigan in any aspect of life. As far as
+          the entertainment industry is concerned, I am intrigued by the
+          transformative potential of technology. From revolutionizing content
+          creation and distribution to reshaping audience engagement, I believe
+          there are still many untapped opportunities to be explored. Currently,
+          I am actively seeking SWE/AI Summer internships for 2024, eager to
+          apply my knowledge and skills in a real-world setting. Outside of
+          academics and career pursuits, I am dedicated to developing products
+          for Soul, a digital identity verification platform. Working on this
+          project has allowed me to apply my technical skills in a practical
+          setting, enhancing my understanding of blockchain technology and its
+          applications in the realm of identity verification. I welcome any
+          opportunity to connect and discuss my interests or potential
+          collaborations. I look forward to hearing from you!
+        </p>
+      </div>
     </div>
   );
 }
